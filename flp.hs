ca6822d778b7911903098840dbbdf7cc323f3b92
Adam Venger <adam.venger@firma.seznam.cz>
2020-02-21 18:17:44 +0100

    basic arg parsing

--- flp.hs
+++ flp.hs
@@ -1,4 +1,6 @@
-import System.IO     
+import System.Environment
+import System.Exit
+import System.IO
 
 splitBy delimiter = foldr f [[]] 
     where f c l@(x:xs) | c == delimiter = []:l
@@ -36,23 +38,74 @@ foo x = x
 determinize :: FSA -> FSA
 determinize dka = dka
 
+-- data FSA = FSA {
+--     states::[String],
+--     alphabet::[Char],
+--     start_state::String,
+--     final_states::[String],
+--     rules::[(String, Char, String)]   -- TODO (String, Char, String)
+-- }
+
+-- data State = State String
+--     deriving (Eq, Show, Read)
+
+type State = String
+
 data FSA = FSA {
-    states::[String],
+    states::[State],
     alphabet::[Char],
-    start_state::String,
-    final_states::[String],
-    rules::[(String, Char, String)]   -- TODO (String, Char, String)
-}
+    start_state::State,
+    final_states::[State]
+} deriving (Eq, Show)
 
+-- TODO Read FSA
 
-main = do  
-    contents <- readFile "test.in"
-    let content = splitBy '\n' contents
+-- main = do  
+--     contents <- readFile "test.in"
+--     let content = splitBy '\n' contents
     
-    let rka = FSA{states = splitBy ',' (content !! 0), alphabet = content !! 1, start_state = content !! 2, final_states = splitBy ',' (content !! 3), rules = splitBy ',' (drop 4 content)}
-    -- let rka = (splitBy ',' (content !! 0), content !! 1, content !! 2, splitBy ',' (content !! 3), drop 4 content)
+--     let rka = FSA{states = splitBy ',' (content !! 0), alphabet = content !! 1, start_state = content !! 2, final_states = splitBy ',' (content !! 3)}
+--     -- let rka = (splitBy ',' (content !! 0), content !! 1, content !! 2, splitBy ',' (content !! 3), drop 4 content)
 
-    return rka
-    -- putStrLn contents
-    -- return contents
-    -- putStr contents
+--     let tmp = splitBy ',' (content !! 0)
+--     return tmp
+--     -- putStrLn contents
+--     -- return contents
+--     -- putStr contents
+
+-- parse2fsa :: String -> String
+-- parse2fsa x = x 
+
+-- -- transform algo
+-- rka2dka rka = unlines . reverse . lines rka
+
+-- -- parsing command line arguments
+-- parseArgs ["-i"] = getContents >>= return . parse2fsa
+-- parseArgs ["-t"] = getContents >>= return . rka2dka . parse2fsa
+-- parseArgs ["h"] = usage >> exit
+-- parse fs = concat `fmap` mapM readFile fs
+
+-- helper functions
+usage   = putStrLn "Usage: rka-2-dka [-ith] [file]"
+exit    = exitWith ExitSuccess
+die     = exitWith (ExitFailure 1)
+
+-- -- main
+-- main = getArgs >>= parseArgs >>= putStr . show
+
+getFile :: [String] -> IO String
+getFile ["-i"] = getContents
+getFile ["-t"] = getContents
+getFile ["-i",file] = readFile file
+getFile ["-t",file] = readFile file
+getFile _ = usage >> exit
+
+main = do
+    args <- getArgs
+    content <- getFile args
+    -- if args == "-i":_
+    --     then putStrLn "iiiii"
+    --     else putStrLn "nee"
+    return ()
+    
+    -- fsa <- parseArgs args
