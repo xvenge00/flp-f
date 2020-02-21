0cca76f9ee07de55f24b5dc2418d3b20e179fef1
Adam Venger <adam.venger@firma.seznam.cz>
2020-02-21 18:56:33 +0100

    basic interface and parsing to simplified FSA

--- flp.hs
+++ flp.hs
@@ -6,49 +6,6 @@ splitBy delimiter = foldr f [[]]
     where f c l@(x:xs) | c == delimiter = []:l
                        | otherwise = (c:x):xs
 
--- read first line
--- states <- split by commas
-
--- read second line
--- alphabet <- chars are already separated in char[]
-
--- read third line
--- starts_state <- whole line is state name
--- check that it is contained in list of states
-
--- read fourth line
--- final_states <- get list separated by commas
--- check that all are contained in 
-
--- recoursively read line until EOF
--- on each line call split by commas
--- check states in states
--- check char in alphabet
--- rules <- append every rule
-
--- TODO algoritmus
-
--- zapis
-
--- foo <- readFile "test.in"
-
-foo :: String -> String
-foo x = x
-
-determinize :: FSA -> FSA
-determinize dka = dka
-
--- data FSA = FSA {
---     states::[String],
---     alphabet::[Char],
---     start_state::String,
---     final_states::[String],
---     rules::[(String, Char, String)]   -- TODO (String, Char, String)
--- }
-
--- data State = State String
---     deriving (Eq, Show, Read)
-
 type State = String
 
 data FSA = FSA {
@@ -58,41 +15,33 @@ data FSA = FSA {
     final_states::[State]
 } deriving (Eq, Show)
 
--- TODO Read FSA
+-- TODO
+-- data FSA = FSA {
+--     states::[State],
+--     alphabet::[Char],
+--     start_state::State,
+--     final_states::[State],
+--     rules::[(State, Char, State)]
+-- } deriving (Eq, Show)
 
--- main = do  
---     contents <- readFile "test.in"
---     let content = splitBy '\n' contents
-    
---     let rka = FSA{states = splitBy ',' (content !! 0), alphabet = content !! 1, start_state = content !! 2, final_states = splitBy ',' (content !! 3)}
---     -- let rka = (splitBy ',' (content !! 0), content !! 1, content !! 2, splitBy ',' (content !! 3), drop 4 content)
+parse2FSA :: String -> FSA
+-- TODO dopln pravidla
+-- TODO dopln kontrolu na to aby to bolo konzistentne
+parse2FSA repr = 
+    let lines = splitBy '\n' repr
+    in FSA{states = splitBy ',' (lines !! 0), alphabet = lines !! 1, start_state = lines !! 2, final_states = splitBy ',' (lines !! 3)}
 
---     let tmp = splitBy ',' (content !! 0)
---     return tmp
---     -- putStrLn contents
---     -- return contents
---     -- putStr contents
+determinize :: FSA -> FSA
+-- TODO
+determinize dka = FSA ["staaaaaav1", "s2"] "abc" "s1" ["s1","s2"]
 
--- parse2fsa :: String -> String
--- parse2fsa x = x 
-
--- -- transform algo
--- rka2dka rka = unlines . reverse . lines rka
-
--- -- parsing command line arguments
--- parseArgs ["-i"] = getContents >>= return . parse2fsa
--- parseArgs ["-t"] = getContents >>= return . rka2dka . parse2fsa
--- parseArgs ["h"] = usage >> exit
--- parse fs = concat `fmap` mapM readFile fs
 
 -- helper functions
 usage   = putStrLn "Usage: rka-2-dka [-ith] [file]"
 exit    = exitWith ExitSuccess
 die     = exitWith (ExitFailure 1)
 
--- -- main
--- main = getArgs >>= parseArgs >>= putStr . show
-
+-- parsing command line arguments
 getFile :: [String] -> IO String
 getFile ["-i"] = getContents
 getFile ["-t"] = getContents
@@ -100,12 +49,11 @@ getFile ["-i",file] = readFile file
 getFile ["-t",file] = readFile file
 getFile _ = usage >> exit
 
+-- main
 main = do
     args <- getArgs
     content <- getFile args
-    -- if args == "-i":_
-    --     then putStrLn "iiiii"
-    --     else putStrLn "nee"
-    return ()
-    
-    -- fsa <- parseArgs args
+    let fsa = if head args == "-t"
+                then (determinize . parse2FSA) content
+                else parse2FSA content
+    putStrLn $ show fsa
