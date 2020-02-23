9e2a0822501ea2b86f5c58938a667effab5cddff
Adam Venger <adam.venger@firma.seznam.cz>
2020-02-23 18:11:53 +0100

    show of states

--- flp.hs
+++ flp.hs
@@ -14,7 +14,18 @@ removeItem x (y:ys) | x == y    = removeItem x ys
                     | otherwise = y : removeItem x ys
 
 -- type definitions
-type State = String
+newtype State = State String
+    deriving (Eq)
+instance Show State where
+    show (State s) = s
+    -- showList rs = (show rs) (++)
+
+newtype States = States [State]
+    deriving (Eq)
+instance Show States where
+    show (States []) = ""
+    show (States [s]) = show s
+    show (States ls) = show (head ls) ++ "," ++ show (States (tail ls))
 
 data Rule = Rule State Char State
     deriving (Eq, Show)
@@ -22,8 +33,8 @@ data Rule = Rule State Char State
 type Alphabet = [Char]
 
 parseRule :: [String] -> Maybe Rule
-parseRule [state, [char], stateNext] = Just $ Rule state char stateNext
-parseRule [state, [], stateNext] = Just $ Rule state '-' stateNext
+parseRule [state, [char], stateNext] = Just $ Rule (parseState state) char (parseState stateNext)
+parseRule [state, [], stateNext] = Just $ Rule (parseState state) '-' (parseState stateNext)
 parseRule _ = Nothing
 
 parseRulesImpl :: [String] -> [Maybe Rule]
@@ -35,38 +46,46 @@ parseRules :: [String] -> Maybe [Rule]
 parseRules lines = sequence $ parseRulesImpl lines
     
 parseState :: String -> State
-parseState state = state
+parseState state = State state
 
-parseStates :: String -> [State]
-parseStates states = map parseState (splitBy ',' states)
+parseStates :: String -> States
+parseStates states = States $ map parseState (splitBy ',' states)
 
 parseAlphabet :: String -> Alphabet
 parseAlphabet alphabet = alphabet
 
 data FSA = FSA {
-    states::[State],
+    states::States,
     alphabet::Alphabet,
     start_state::State,
-    final_states::[State],
+    final_states::States,
     rules::[Rule]
-} deriving (Eq, Show)
+} deriving (Eq)
+
+-- instance Show State where
+--   show (String x) = show x
+
+--   showList rs = unlines (map show rs)
+
+instance Show FSA where
+  show (FSA state alphabet start_state final_states rules) = show state ++ "\n" ++ show alphabet ++ "\n" ++ show start_state ++ "\n" ++ show final_states ++ "\n" ++ show rules
 
 
 -- TODO dopln kontrolu na to aby to bolo konzistentne
 parse2FSA :: String -> Maybe FSA
 parse2FSA repr = do
     let lines = splitBy '\n' repr
-    let states = parseStates (lines !! 0)
-    let alphabet = parseAlphabet (lines !! 1)
-    let start_state = parseState (lines !! 2)
-    let final_states = parseStates (lines !! 3)
+        states = parseStates (lines !! 0)
+        alphabet = parseAlphabet (lines !! 1)
+        start_state = parseState (lines !! 2)
+        final_states = parseStates (lines !! 3)
     rules <- parseRules (removeItem "" $ drop 4 lines)  -- TODO maybe remove only last element?
     Just $ FSA states alphabet start_state final_states rules
 
 
 determinize :: FSA -> FSA
 -- TODO
-determinize dka = FSA ["staaaaaav1", "s2"] "abc" "s1" ["s1","s2"] [Rule "a" 'a' "a"]
+determinize dka = FSA (States [State "staaaaaav1", State "s2"]) "abc" (State "s1") (States [State "s1", State "s2"]) [Rule (State "a") 'a' (State "a")]
 
 
 -- helper functions
